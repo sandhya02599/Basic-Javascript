a3405989369e4ad802e3086a47f211b231d62111
sandhya02599 <sandhya02599@gmail.com>
2024-07-18 13:32:58 +0530

    Basic javascript

---
+++ project.js
@@ -0,0 +1,63 @@
+// // variable:
+
+// var sandhya= ("singing")
+// console.log(sandhya)
+
+
+// var Hello=("World")
+// console.log(Hello)
+
+
+// // let:
+
+// let ravi = 'playing'
+// console.log(ravi)
+
+// ravi = 'ground'
+// console.log(ravi)
+
+// // const:
+
+// const ground = 'stadium'
+// console.log(ground)
+
+
+// // Data Tayes:
+
+// var a =("Enter a message")
+// console.log(a)
+
+
+// console.log(103050807)
+
+// Boolean = true, false
+
+// test= 30 > 40;
+// console.log(test)
+
+// test= 40 < 50;
+// console.log(test)
+
+//____________________________________________//
+
+
+document.write("This is a text message <br><br>")
+
+
+document.write(1234234117866)
+
+document.write("<h1>Data Types</h1>")
+
+// Arrays:
+ 
+let color = ["red", "pink", 67, {name: "Radha"}]
+console.log(color)
+
+//object:
+
+const games = {
+    volleyball: "ravi",
+    tennis: "lakshmi"
+}
+
+console.log(games)
